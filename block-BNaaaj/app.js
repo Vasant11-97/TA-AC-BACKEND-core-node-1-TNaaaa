51937ef609fe817f0cbaa89add6f6e3133be1795
Vasant Saraswat <vasant.saraswat@gmail.com>
2021-12-17 18:48:08 +0530

    Assignment 1 of core node is completed

---
+++ block-BNaaaj/app.js
@@ -0,0 +1,8 @@
+var area = require('./area');
+
+var circleAera = area.circle(13);
+
+console.log(circleAera);
+var buffer = Buffer.alloc(12);
+buffer.write('Hello Vasant !');
+console.log(buffer.toString());
